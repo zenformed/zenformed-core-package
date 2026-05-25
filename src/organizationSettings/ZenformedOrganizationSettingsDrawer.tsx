79fc199a8e9fec6200eb4db497de7576f3b7b635
Daniel Henderson <djhindu17@gmail.com>
2026-05-25 13:11:59 -0500

    package update

--- src/organizationSettings/ZenformedOrganizationSettingsDrawer.tsx
+++ src/organizationSettings/ZenformedOrganizationSettingsDrawer.tsx
@@ -1,19 +1,38 @@
-'use client';
-
-import {
-  ZenformedOrganizationSettingsOverlay,
-  type ZenformedOrganizationSettingsOverlayProps,
-} from './ZenformedOrganizationSettingsOverlay';
-
-/** @deprecated Use {@link ZenformedOrganizationSettingsOverlay}. Drawer classNames are ignored. */
-export type ZenformedOrganizationSettingsDrawerProps = ZenformedOrganizationSettingsOverlayProps & {
-  readonly classNames?: unknown;
-};
-
-/** @deprecated Use {@link ZenformedOrganizationSettingsOverlay}. */
-export function ZenformedOrganizationSettingsDrawer({
-  classNames: _classNames,
-  ...props
-}: ZenformedOrganizationSettingsDrawerProps) {
-  return <ZenformedOrganizationSettingsOverlay {...props} />;
-}
+'use client';
+
+
+
+import {
+
+  ZenformedOrganizationSettingsOverlay,
+
+  type ZenformedOrganizationSettingsOverlayProps,
+
+} from './ZenformedOrganizationSettingsOverlay';
+
+
+
+/** @deprecated Use {@link ZenformedOrganizationSettingsOverlay}. Drawer classNames are ignored. */
+
+export type ZenformedOrganizationSettingsDrawerProps = ZenformedOrganizationSettingsOverlayProps & {
+
+  readonly classNames?: unknown;
+
+};
+
+
+
+/** @deprecated Use {@link ZenformedOrganizationSettingsOverlay}. */
+
+export function ZenformedOrganizationSettingsDrawer({
+
+  classNames: _classNames,
+
+  ...props
+
+}: ZenformedOrganizationSettingsDrawerProps) {
+
+  return <ZenformedOrganizationSettingsOverlay {...props} />;
+
+}
+
